444ad500037383a1ce8ef2e43e5883555d1e4440
nakometa <Killerawe1>
2020-11-30 18:09:55 +0200

    My solutions of the homework during the course Programming Basics - July 2020

---
+++ Programming Basics - July 2020/First Steps In Coding - Lab/03.Square Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int area = num * num;
+
+            Console.WriteLine(area);
+        }
+    }
+}
