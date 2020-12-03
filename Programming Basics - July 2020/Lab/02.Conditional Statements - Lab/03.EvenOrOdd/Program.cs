ef9b1521ec766a0952b98ecf4ef6d838d5aecbfd
nakometa <kruseryyy@gmail.com>
2020-12-03 12:38:25 +0200

    Added more solutions and ordered course work in descending order

---
+++ Programming Basics - July 2020/Lab/02.Conditional Statements - Lab/03.EvenOrOdd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
