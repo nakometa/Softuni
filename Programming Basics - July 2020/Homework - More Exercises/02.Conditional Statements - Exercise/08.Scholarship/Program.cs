be89f28873c9956a19b467cf0c967907948015af
nakometa <kruseryyy@gmail.com>
2020-12-04 16:47:34 +0200

    Added more solutions of my homework

---
+++ Programming Basics - July 2020/Homework - More Exercises/02.Conditional Statements - Exercise/08.Scholarship/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08.Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            double income = double.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            double minimalWage = double.Parse(Console.ReadLine());
+
+            //Calculations
+            double socialScholarship = Math.Floor(minimalWage * 0.35);
+            double excellentScholarship = Math.Floor(averageGrade * 25);
+
+            //Output
+            if (income >= minimalWage && averageGrade >= 5.5)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results {excellentScholarship} BGN");
+            }
+            else if (income >= minimalWage && averageGrade < 5.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else if (income < minimalWage && averageGrade >= 5.5 && socialScholarship <= excellentScholarship)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results {excellentScholarship} BGN");
+            }
+            else if (income < minimalWage && averageGrade > 4.5)
+            {
+                Console.WriteLine($"You get a Social scholarship {socialScholarship} BGN");
+            }
+            else if (income < minimalWage && averageGrade <= 4.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}
