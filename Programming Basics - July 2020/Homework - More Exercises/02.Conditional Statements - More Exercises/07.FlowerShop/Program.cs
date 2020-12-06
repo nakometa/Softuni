305fd18a9a51af6082a5f53985a40870e61bec94
nakometa <kruseryyy@gmail.com>
2020-12-06 11:16:03 +0200

    Added more solutions

---
+++ Programming Basics - July 2020/Homework - More Exercises/02.Conditional Statements - More Exercises/07.FlowerShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            int magnoliasCount = int.Parse(Console.ReadLine());
+            int hyacinthsCount = int.Parse(Console.ReadLine());
+            int rosesCount = int.Parse(Console.ReadLine());
+            int cactusCount = int.Parse(Console.ReadLine());
+            double giftPrice = double.Parse(Console.ReadLine());
+
+            // Calculations
+            double orderPrice = (magnoliasCount * 3.25) + (hyacinthsCount * 4) + (rosesCount * 3.50) + (cactusCount * 8);
+            double profitAfterTax = orderPrice - (orderPrice * 0.05);
+
+            // Output
+            if (profitAfterTax >= giftPrice)
+            {
+                double moneyLeft = Math.Floor(profitAfterTax - giftPrice);
+                Console.WriteLine($"She is left with {moneyLeft} leva.");
+            }
+            else
+            {
+                double borrowNeeded = Math.Ceiling(giftPrice - profitAfterTax);
+                Console.WriteLine($"She will have to borrow {borrowNeeded} leva.");
+            }
+        }
+    }
+}
---
+++ Programming Basics - July 2020/Homework - More Exercises/02.Conditional Statements - More Exercises/08. FuelTank-Part2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace _08._FuelTank_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            double fuelAmount = double.Parse(Console.ReadLine());
+            string discountCard = Console.ReadLine();
+
+            // Calculations
+            double totalPrice = 0;
+            double finalPrice = 0;
+
+            switch (fuelType)
+            {
+                case "Gas":
+                    if (discountCard == "Yes" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * (0.93 - 0.08);
+                    }
+                    else if (discountCard == "Yes" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * (0.93 - 0.08);
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "Yes" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * (0.93 - 0.08);
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * 0.93;
+                    }
+                    else if (discountCard == "No" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * 0.93;
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * 0.93;
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    break;
+                case "Gasoline":
+                    if (discountCard == "Yes" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * (2.22 - 0.18);
+                    }
+                    else if (discountCard == "Yes" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * (2.22 - 0.18);
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "Yes" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * (2.22 - 0.18);
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * 2.22;
+                    }
+                    else if (discountCard == "No" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * 2.22;
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * 2.22;
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    break;
+                case "Diesel":
+                    if (discountCard == "Yes" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * (2.33 - 0.12);
+                    }
+                    else if (discountCard == "Yes" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * (2.33 - 0.12);
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "Yes" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * (2.33 - 0.12);
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount < 20)
+                    {
+                        finalPrice = fuelAmount * 2.33;
+                    }
+                    else if (discountCard == "No" && fuelAmount >= 20 && fuelAmount <= 25)
+                    {
+                        totalPrice = fuelAmount * 2.33;
+                        double discount = totalPrice * 0.08;
+                        finalPrice = totalPrice - discount;
+                    }
+                    else if (discountCard == "No" && fuelAmount > 25)
+                    {
+                        totalPrice = fuelAmount * 2.33;
+                        double discount = totalPrice * 0.1;
+                        finalPrice = totalPrice - discount;
+                    }
+                    break;
+            }
+            Console.WriteLine($"{finalPrice:F2} lv.");
+        }
+    }
+}
