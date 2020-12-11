4cac6e4b0fc2fece395d82131e7bc41811f3c542
nakometa <kruseryyy@gmail.com>
2020-12-11 17:05:33 +0200

    Added solutions

---
+++ Programming Basics - July 2020/Lab/04.For Loop - Lab/02.NumbersToN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.NumbersToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; n--)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
