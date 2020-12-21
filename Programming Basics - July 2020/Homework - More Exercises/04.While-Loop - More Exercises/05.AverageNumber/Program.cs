45340c69868b05f8e418c439d37076ad9139707b
nakometa <kruseryyy@gmail.com>
2020-12-21 11:42:41 +0200

    Added solutions

---
+++ Programming Basics - July 2020/Homework - More Exercises/04.While-Loop - More Exercises/05.AverageNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine($"{sum / n:F2}");
+        }
+    }
+}
