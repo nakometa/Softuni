f3c9c452b9ea956395eb087f8287af62a3c5ac38
nakometa <kruseryyy@gmail.com>
2021-01-05 12:16:17 +0200

    Added solutions.

---
+++ Fundamentals C# - September 2020/Lab/05.Lists - Lab/02.Gauss'Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Gauss_Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                         .Split(" ")
+                         .Select(int.Parse)
+                         .ToList();
+            int originalLength = input.Count;
+
+            for (int i = 0; i < originalLength / 2; i++)
+            {
+                input[i] += input[input.Count - 1];
+                input.RemoveAt(input.Count - 1);
+            }
+
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
---
+++ Fundamentals C# - September 2020/Lab/05.Lists - Lab/04.ListOfProducts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> vegetables = new List<string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                vegetables.Add(currentProduct);
+            }
+            vegetables.Sort();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{vegetables[i]}");
+            }
+        }
+    }
+}
