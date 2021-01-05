5a5656357b5fe41997eab084dcdc22f8d8258b77
nakometa <kruseryyy@gmail.com>
2021-01-05 12:17:19 +0200

    Added solutions.

---
+++ Fundamentals C# - September 2020/Lab/05.Lists - Lab/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (numbers.Count > 0)
+            {
+                numbers.Reverse();
+                Console.WriteLine(String.Join(" ", numbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
---
+++ Fundamentals C# - September 2020/Lab/05.Lists - Lab/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                            .Split(" ")
+                            .Select(int.Parse)
+                            .ToList();
+            //
+            string[] command = Console.ReadLine()
+                                       .ToLower()
+                                      .Split(" ");
+
+            while (command[0] != "end")
+            {
+                int num = int.Parse(command[1]);
+
+                switch (command[0])
+                {
+                    case "add":
+                        numbers.Add(num);
+                        break;
+                    case "remove":
+                        numbers.Remove(num);
+                        break;
+                    case "removeat":
+                        numbers.RemoveAt(num);
+                        break;
+                    case "insert":
+                        int index = int.Parse(command[2]);
+                        numbers.Insert(index, num);
+                        break;
+                }
+                command = Console.ReadLine()
+                                  .ToLower()
+                                 .Split(" ");
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
