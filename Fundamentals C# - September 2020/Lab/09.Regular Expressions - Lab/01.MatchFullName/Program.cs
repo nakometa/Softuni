7e187e1998eb125d8346408875c3f9eaf3497d68
nakometa <kruseryyy@gmail.com>
2021-01-16 23:29:15 +0200

    Added solutions.

---
+++ Fundamentals C# - September 2020/Lab/09.Regular Expressions - Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z]{1}[a-z]+ \b[A-Z]{1}[a-z]+";
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, pattern);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fundamentals C# - September 2020/Lab/09.Regular Expressions - Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"[+359]{4}( |-)2\1[0-9]{3}\1[0-9]{4}\b";
+            var phones = Console.ReadLine();
+            var matchPhones = Regex.Matches(phones, pattern);
+
+            var matchedPhones = matchPhones.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(String.Join(", ", matchedPhones));
+        }
+    }
+}
