48615af73507150b74f1619fc8619d591ad4c990
nakometa <kruseryyy@gmail.com>
2021-03-12 21:15:49 +0200

    Added solutions.

---
+++ C# OOP - February 2021/Homework/01.Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : 
+            base(name, age)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
